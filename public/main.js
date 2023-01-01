2c3b5a52afef5f5777772064d8bf65279c8910df
sangjun-ray <>
2023-01-01 19:11:14 +0900

    setting: react + electron 세팅

---
+++ public/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+  });
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+
+  win.loadURL(startUrl);
+}
+
+app.on("ready", createWindow);
