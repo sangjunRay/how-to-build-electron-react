eee2fc6283637da0921de3245a2e32ca10c89154
Ray <raypark970421@gmail.com>
2023-01-01 19:40:13 +0900

    sourcetree push test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Its Ray first Electron App</h1>
       <p>I'll lunching my game in steam!</p>
       <p>testtest</p>
+      <p>sourcetree push test</p>
     </div>
   );
 }
