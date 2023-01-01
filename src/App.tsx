11b54f36356155f08d521d6ee1a522f5ba29cc67
sangjun-ray <>
2023-01-01 19:32:22 +0900

    ssh test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <h1>Its Ray first Electron App</h1>
+      <p>I'll lunching my game in steam!</p>
+    </div>
+  );
 }
 
 export default App;
